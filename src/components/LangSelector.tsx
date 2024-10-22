0f7443c83cad02143b4c85d83e9810471f85063f
Samyr Ribeiro <saamyr@live.com>
2024-10-21 23:05:38 -0300

    refactor: refactored lang selector component, style and code

--- src/components/LangSelector.tsx
+++ src/components/LangSelector.tsx
@@ -1,61 +1,41 @@
-const slugMapping = {
-  en: {
-    posts: {
-      "como-integrar-giscus": "how-to-integrate-giscus-comments", // Portuguese equivalent slug
-      // Add more slugs here as needed
-    },
-  },
-  "pt-br": {
-    posts: {
-      "how-to-integrate-giscus-comments": "como-integrar-giscus", // Portuguese equivalent slug
-      // Add more slugs here as needed
-    },
-  },
-  // Add more languages and their respective slugs
-};
+import { slugsMapping } from "i18n/slugsMapping";
+
 export default function LangSelector() {
   const currentLang = window.location.pathname.startsWith("/pt-br")
     ? "pt-br"
     : "en";
-  // Function to handle language switch
-  const switchLanguage = event => {
+
+  const switchLanguage = (event: React.ChangeEvent<HTMLSelectElement>) => {
     const newLang = event.target.value;
     const currentUrl = window.location.pathname;
-
-    // Split the URL to get the language part (assuming /[lang]/path/structure)
     const parts = currentUrl.split("/");
-    //
-    // Get the current slug from the URL
-    const currentSlug = parts[3]; // Assuming slug is at index 2
-
-    // Determine the new slug based on the selected language
-    const newSlug = slugMapping[newLang]?.posts[currentSlug] || currentSlug;
-
-    // Replace the language segment with the selected one
+    const currentSlug = parts[3];
+    const newSlug = slugsMapping[newLang]?.posts[currentSlug] || currentSlug;
     if (parts[1]) {
-      parts[1] = newLang; // Update to the selected language
+      parts[1] = newLang;
     }
-
     if (parts[3]) {
-      parts[3] = newSlug; // Replace the slug with the new slug
+      parts[3] = newSlug;
     }
-
-    // Join the parts back into a full URL
     const newUrl = parts.join("/");
-
-    // Reload the page with the new URL
     window.location.href = newUrl;
   };
 
   return (
-    <select
-      onChange={switchLanguage}
-      aria-label="Language Selector"
-      value={currentLang}
-    >
-      <option value="en">en</option>
-      <option value="pt-br">pt-br</option>
-      {/* Add more languages here if needed */}
-    </select>
+    <>
+      <select
+        onChange={switchLanguage}
+        aria-label="Language Selector"
+        value={currentLang}
+        className="lang-selector bg-transparent pr-1 font-medium hover:text-skin-accent"
+      >
+        <option className="lang-selector__opt" value="en">
+          en-us
+        </option>
+        <option className="lang-selector__opt" value="pt-br">
+          pt-br
+        </option>
+      </select>
+    </>
   );
 }
