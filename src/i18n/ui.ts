805748b08e09b0a5c60c1998a6a4f850e7ba618f
Samyr Ribeiro <saamyr@live.com>
2024-11-06 22:53:08 -0300

    feat(i18n): located index page

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -1,12 +1,29 @@
 export const ui = {
   en: {
-    "site.title": "Code & Craft",
+    "index.welcome": "Welcome!",
+    "index.links": "Social Links:",
+    "index.featured": "Featured",
+    "index.recents": "Recent Posts",
+    "index.allPosts": "All Posts",
+    "index.header.first":
+      "It's a pleasure to have you on this journey! Here, you'll find technical articles, open-source contributions, and creative projects.",
+    "index.header.second":
+      "Let’s dive in to build, learn, and share experiences about technology! 🤓",
     "datetime.updated": "Updated:",
     "search.title": "Search",
     "search.anything": "Search for anything...",
   },
   "pt-br": {
-    "site.title": "Codar & Criar",
+    "index.welcome": "Bem vindo!",
+    "index.links": "Redes: ",
+    "index.featured": "Destaques",
+    "index.recents": "Posts recents",
+    "index.allPosts": "Todos os posts",
+    "index.header.first":
+      "É um prazer ter você nessa jornada, aqui você encontrará artigos técnicos, contribuições open source, e projetos criativos!",
+    "index.header.second":
+      "Vamos nessa contruir, aprender e compartilhar experiências sobre tecnologia! 🤓",
+
     "datetime.updated": "Última atualização:",
     "search.title": "Buscar",
     "search.anything": "Busque por qualquer coisa...",
