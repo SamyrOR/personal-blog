946121738a806dcee896009f014ce51ecf017efe
Samyr Ribeiro <saamyr@live.com>
2024-11-10 14:02:58 -0300

    feat(i18n): located footer and header components

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -1,5 +1,6 @@
 export const ui = {
   en: {
+    //Pages
     "index.welcome": "Welcome!",
     "index.links": "Social Links:",
     "index.featured": "Featured",
@@ -9,11 +10,16 @@ export const ui = {
       "It's a pleasure to have you on this journey! Here, you'll find technical articles, open-source contributions, and creative projects.",
     "index.header.second":
       "Let’s dive in to build, learn, and share experiences about technology! 🤓",
-    "datetime.updated": "Updated:",
     "search.title": "Search",
     "search.anything": "Search for anything...",
+    //Components
+    "header.skip": "Skip to content",
+    "header.about": "About",
+    "footer.rights": "All rights reserved.",
+    "datetime.updated": "Updated:",
   },
   "pt-br": {
+    //Pages
     "index.welcome": "Bem vindo!",
     "index.links": "Redes: ",
     "index.featured": "Destaques",
@@ -23,9 +29,12 @@ export const ui = {
       "É um prazer ter você nessa jornada, aqui você encontrará artigos técnicos, contribuições open source, e projetos criativos!",
     "index.header.second":
       "Vamos nessa contruir, aprender e compartilhar experiências sobre tecnologia! 🤓",
-
-    "datetime.updated": "Última atualização:",
     "search.title": "Buscar",
     "search.anything": "Busque por qualquer coisa...",
+    //Components
+    "header.skip": "Pular para o conteúdo",
+    "header.about": "Sobre",
+    "footer.rights": "Todos os direitos reservados.",
+    "datetime.updated": "Última atualização:",
   },
 };
